01ca0afb823aa6665acd5a2be02855c9558c39ea
hydrostaticcog <jett6440@gmail.com>
2020-12-07 11:31:25 -0600

    changes spell parsing from a json file in spell.js into a git submodule

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-if (Number(process.version.slice(1).split(".")[0]) < 12) throw new Error("Node 12.0.0 or higher is required. Update Node on your system.");
-
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
@@ -9,18 +7,26 @@ const config = require("./config.json");
 client.config = config;
 Logger = require('./modules/logger');
 client.logger = new Logger('main');
+client.commands = new Enmap();
+client.aliases = new Enmap();
+
+let GHversion = require('./package.json')
+client.logger.info(`Starting Grick Heart version ${GHversion.version} running Node version ${(Number(process.version.slice(1).split(".")[0]))}`);
+if (Number(process.version.slice(1).split(".")[0]) < 12) {
+  let errorN = new Error("Node 12.0.0 or higher is required. Update Node on your system.");
+  client.logger.error(errorN)
+}
 
 fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
+  if (err) return client.logger.error(err);
   files.forEach(file => {
+    client.logger.info(`Attempting to load event ${file}`);
     const event = require(`./events/${file}`);
     let eventName = file.split(".")[0];
     client.on(eventName, event.bind(null, client));
+    client.logger.info(`Successfully loaded event ${file}`);
   });
 });
-
-client.commands = new Enmap();
-client.aliases = new Enmap();
 
 fs.readdir("./commands/", (err, files) => {
   if (err) return client.logger.error(err);
